e7efd0dc396fa5f0e89a3a4b18fad0faa93dcd1b
Michael Myers <michaelzmyers@gmail.com>
2020-03-03 20:58:35 -0800

    Added hashtags to links

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,10 +5,68 @@ import Avatar from '@material-ui/core/Avatar'
 import Typography from '@material-ui/core/Typography'
 import useInterval from '@use-it/interval';
 import linkifyHtml from 'linkifyjs/html'
+import * as linkify from 'linkifyjs';
+import hashtag from 'linkifyjs/plugins/hashtag';
 import { withStyles } from '@material-ui/core/styles'
+hashtag(linkify);
 
 const formatTime = time => {
   return time.fromNow()
+}
+
+// const formatMessage = message => {
+//   return linkifyHtml(message.text, {
+//     defaultProtocol: 'https',
+//     // formatHref: function (href, type) {
+//     //   console.log(href, type)
+//     //   // if (type === 'hashtag') {
+//     //   //   href = `${process.env.REACT_APP_ROOT_URL}/synapse/${href.substring(1)}`
+//     //   // }
+//     //   return href;
+//     // }
+//   });
+// }
+
+const htmlify = text => {
+  return linkifyHtml(text, {
+    defaultProtocol: 'https',
+    formatHref: function (href, type) {
+      if (type === "hashtag") {
+        href = process.env.REACT_APP_LINK_ROOT + '/synapse/' + href.substring(1)
+      }
+      if (type === "url" && href.includes(process.env.REACT_APP_LINK_ROOT)) {
+        let parts = href.split('/')
+        href = `${process.env.REACT_APP_LINK_ROOT}/synapse/${parts[parts.length-1]}`
+      }
+      return href
+    },
+    format: function (value, type) {
+      if (type === "url" && value.includes(process.env.REACT_APP_LINK_ROOT)) {
+        let parts = value.split('/')
+        value = `#${parts[parts.length-1]}`
+      }
+      return value
+    },
+    className: function (href, type) {
+      if (type === "hashtag") {
+        return 'linkified-hashtag'
+      }
+      if (type === "url" && href.includes(process.env.REACT_APP_LINK_ROOT)) {
+        return 'linkified-hashtag'
+      }
+      return 'linkified'
+    },
+    target: function(value, type) {
+      let target = '_blank'
+      if (type === "url" && value.includes(process.env.REACT_APP_LINK_ROOT)) {
+        target =  null
+      }
+      if (type === "hashtag") {
+        target = null
+      }
+      return target
+    }
+  })
 }
 
 const Message = props => {
@@ -21,10 +79,6 @@ const Message = props => {
   }, 60000)
 
   const userImage = `https://api.adorable.io/avatars/64/${message.user}`
-
-  const messageHtml = linkifyHtml(message.text, {
-    defaultProtocol: 'https'
-  });
 
   return (
     <div
@@ -41,7 +95,7 @@ const Message = props => {
         alt={message.user}
       />
       <div>
-        <Typography>{parse(messageHtml)}</Typography>
+        <Typography>{parse(htmlify(message.text))}</Typography>
         <Typography variant="caption" style={{color: "#666"}}>{message.delivered ? `${message.user.substring(message.user.length - 5)} - ${time}` : 'Sending...'}</Typography>
       </div>
     </div>
