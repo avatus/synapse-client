3f7c412cf9c7c403cd90fba7435a2b44d23949f0
Michael Myers <michaelzmyers@gmail.com>
2020-03-09 18:22:19 -0700

    Removed redux dev tool from production

--- src/config/redux-store.js
+++ src/config/redux-store.js
@@ -3,7 +3,13 @@ import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
 
 export default function configureStore () {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  let composeEnhancers
+  if (process.env.REACT_APP_ENV === "dev") {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  }
+  else {
+    composeEnhancers = compose
+  }
     return createStore(rootReducer, composeEnhancers(
         applyMiddleware(thunk)
   ));
